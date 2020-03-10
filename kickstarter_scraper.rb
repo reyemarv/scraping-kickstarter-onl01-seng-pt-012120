aebe38f6aef6cfbdcf4e24e55af8be67ce7a4e45
Marvin Reyes <reyemarv@gmail.com>
2020-03-10 04:08:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,9 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text 
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = { 
+      :image_link =>
+    }
   end 
   
   projects 
